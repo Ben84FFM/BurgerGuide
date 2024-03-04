76bd8690aec8df42235eaf66731492f1f9685da2
Ben84FFM <149787249+Ben84FFM@users.noreply.github.com>
2024-03-04 19:42:59 +0100

    modiefiedNavbarFooterAboutUs

--- client/src/Components/AboutUs.jsx
+++ client/src/Components/AboutUs.jsx
@@ -22,21 +22,36 @@ return (
       {/* Container mit dem Text und dem Bild */}
       <div className="containerLogoTextBox absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col items-center bg-black border-slate-50 rounded-xl shadow-xl shadow-gray-500">
         <img
-          src="../src/assets/BurgerGuideLogo3.png"
+          src="../src/assets/BurgerGuideLogo2.png"
           alt="BurgerGuideLogo"
           className="w-1/2 h-auto rounded-md mb-4"
         />
         <div className="containerTextBox text-center">
-          <h1 className="text-cbb26a text-lg font-bold mb-4">BurgerGuide</h1>
+          <h1 className="text-cbb26a text-lg font-bold mb-4">Welcome to BurgerGuide!</h1>
           <p className="text-cbb26a mb-4">
-            Your passion for burgers has inspired us to create this platform where you can discover the best burger
-            locations in your city.
+          At BurgerGuide, it's all about the passion for great food and creating a platform that brings burger enthusiasts from around the world together. Our mission is to discover the best burger joints and share them with the community.
           </p>
           <p className="text-cbb26a mb-4">
-            Immerse yourself in the world of burgers with transparent reviews from the burger community.
+          Our developers at BurgerGuide rely on the MERN stack, specifically tailored to the needs of modern web applications. Here are the main components of our technology stack: MongoDB & Mongoose. </p>
+          <p className="text-cbb26a mb-4">For efficient database management, we utilize MongoDB and Mongoose. These powerful technologies allow us to store and retrieve extensive information about burger joints. Express.js Express.js forms the backbone of our server-side development. With this robust Node.js framework, we can quickly and efficiently create APIs to exchange data between our database and the frontend. React.js
           </p>
           <p className="text-cbb26a mb-4">
-            BurgerGuide - Where passion meets taste. Discover, rate, and share your burger experiences today!
+          Our frontend is developed using React.js to ensure a responsive and engaging user interface. This allows users to effortlessly navigate through different burger joints and discover their favorites. 
+          <p className="text-cbb26a mb-4">
+          Axios For seamless communication between our frontend and backend, we rely on Axios. This HTTP client library facilitates data exchange and contributes to a smooth user experience.
+          Tailwind CSS </p>
+          The layout and design of our website are crafted with Tailwind CSS. This powerful CSS framework enables us to quickly and efficiently create attractive user interfaces that are easy to maintain.
+          <p className="text-cbb26a mb-4">
+          Bcrypt: The security of our user data is paramount. Therefore, we use Bcrypt to securely encrypt passwords and ensure the integrity of user accounts.
+          </p>
+
+          <p className="text-cbb26a mb-4">
+          We take pride in developing BurgerGuide with this advanced technology stack and look forward to exploring the world of burgers with you!
+          </p>
+
+          <p className="text-cbb26a mb-4">
+          Welcome to BurgerGuide - where burger dreams come true!
+            </p>
           </p>
         </div>
       </div>
--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -4,28 +4,28 @@ const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="bg-black text-cbb26a py-8">
-      <div className="container mx-auto flex flex-wrap justify-center lg:justify-between">
-        <div className="footer-section mb-4 lg:mb-0">
-          <h4 className="text-cbb26a text-lg font-bold">Impressum</h4>
-          <p className="text-cbb26a">BurgerGuide</p>
-          <p className="text-cbb26a">WBS WDG14</p>
-          <p className="text-cbb26a">Email</p>
-          <p className="text-cbb26a">Telefonnummer</p>
-          {/* Weitere Impressum-Informationen hier */}
-        </div>
+<footer className="bg-black text-cbb26a py-8">
+  <div className="container mx-auto flex flex-wrap justify-center lg:justify-between">
+    <div className="footer-section mb-4 lg:mb-0">
+      <h4 className="text-cbb26a text-lg font-bold mb-2">Impressum</h4>
+      <p className="text-cbb26a mb-1">BurgerGuide</p>
+      <p className="text-cbb26a mb-1">WBS WDG14</p>
+      <p className="text-cbb26a mb-1">Email</p>
+      <p className="text-cbb26a mb-1">Telefonnummer</p>
+      {/* Weitere Impressum-Informationen hier */}
+    </div>
 
-        <div className="footer-section mb-4 lg:mb-0">
-          <h4 className="text-cbb26a text-lg font-bold" >Copyright © {currentYear}</h4>
-          <p className="text-cbb26a">Alle Rechte vorbehalten.</p>
-        </div>
+    <div className="footer-section mb-4 lg:mb-0">
+      <h4 className="text-cbb26a text-lg font-bold mb-2" >Copyright © {currentYear}</h4>
+      <p className="text-cbb26a mb-1">Alle Rechte vorbehalten.</p>
+    </div>
 
-        <div className="footer-section">
-          <h4 className="text-cbb26a">Datenschutz</h4>
-          <p className="text-cbb26a">Datenschutzbestimmungen hier.</p>
-        </div>
-      </div>
-    </footer>
+    <div className="footer-section">
+      <h4 className="text-cbb26a mb-2">Datenschutz</h4>
+      <p className="text-cbb26a mb-1">Datenschutzbestimmungen hier.</p>
+    </div>
+  </div>
+</footer>
   );
 };
 
