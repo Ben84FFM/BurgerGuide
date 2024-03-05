23ddb9bcc4e6cd3e14b5f7c7b3a27c11a5090bd3
Ben84FFM <149787249+Ben84FFM@users.noreply.github.com>
2024-03-05 11:09:20 +0100

    AdaptationNavbar

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -25,12 +25,12 @@ function Navbar() {
       bg-black p-4 font-bold'>
       <Link to='/' className='cursor-pointer hover:opacity-80        text-lg font-bold text-cbb26a'><h1 className='text-lg '>Home</h1></Link>
       <img src="../src/assets/BurgerGuideLogo2.png" alt="Logo" className="h-8 w-8" />
+      <Link to='/about' className='cursor-pointer hover:opacity-80 text-lg font-bold text-cbb26a '> About</Link>
       <div className="flex flex-grow justify-around items-center space-x-4">
         {isLoggedIn ? (
           <div className='flex flex-row '>
             <p >Welcome {userData.firstName}</p>
-            <Link to='/about' className='cursor-pointer hover:opacity-80 text-lg font-bold text-cbb26a '> About</Link>
-            <Link to='/burgerstores' className='cursor-pointer hover:opacity-80   text-lg font-bold text-cbb26a'><h1 className=''>Stores</h1></Link>
+                        <Link to='/burgerstores' className='cursor-pointer hover:opacity-80   text-lg font-bold text-cbb26a'><h1 className=''>Stores</h1></Link>
             <button className='cursor-pointer hover:opacity-80 text-lg font-bold text-cbb26a ' onClick={handleLogout}>LOGOUT</button>
           </div>
         ) : (
