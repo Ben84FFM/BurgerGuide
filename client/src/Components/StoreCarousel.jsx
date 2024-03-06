f612d0d2443f13088807b26f7af487efce22ec31
Ben84FFM <149787249+Ben84FFM@users.noreply.github.com>
2024-03-06 19:44:05 +0100

    LinkNavigation

--- client/src/Components/StoreCarousel.jsx
+++ client/src/Components/StoreCarousel.jsx
@@ -1,6 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import axios from 'axios';
 import Slider from 'react-slick';
+import { Link } from 'react-router-dom'; // Importiere die Link-Komponente
 
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
@@ -9,12 +10,13 @@ const StoreCarousel = () => {
   const [stores, setStores] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
+  const containerRef = useRef(null);
 
   useEffect(() => {
     const fetchStores = async () => {
       try {
         const response = await axios.get('http://localhost:5000/stores');
-        setStores(response.data.data); // Zugriff auf die 'data'-Eigenschaft des Objekts
+        setStores(response.data.data);
         setLoading(false);
       } catch (error) {
         console.error(error);
@@ -32,7 +34,20 @@ const StoreCarousel = () => {
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
   };
+
+  useEffect(() => {
+    if (containerRef.current) {
+      const maxHeight = Math.max(
+        ...Array.from(containerRef.current.children).map((child) => child.clientHeight)
+      );
+      Array.from(containerRef.current.children).forEach(
+        (child) => (child.style.height = `${maxHeight}px`)
+      );
+    }
+  }, [stores]);
 
   if (loading) {
     return <p className="text-center">Loading...</p>;
@@ -47,17 +62,34 @@ const StoreCarousel = () => {
     return <p className="text-center text-red-500">Error loading stores data.</p>;
   }
 
-
   return (
     <div className="text-center px-4 lg:px-0">
       <h2 className="text-2xl lg:text-3xl xl:text-4xl text-cbb26a font-bold mb-6">Featured Stores</h2>
       <Slider {...settings} className="mx-auto max-w-screen-lg">
         {stores.map((store) => (
-          <div key={store._id} className="px-2 lg:px-4">
-            <div className="bg-white rounded-lg p-4 lg:p-6 shadow-md">
-              <h3 className="text-lg lg:text-xl xl:text-2xl text-cbb26a font-bold mb-2">{store.name}</h3>
-              <p className="text-gray-600 mb-2">{store.phonenumbers}</p>
-              <p className="text-blue-500 mb-2">{store.website}</p>
+          <div key={store._id} className="flex items-center px-2 lg:px-4">
+            <div className="flex bg-white rounded-md p-4 lg:p-6 shadow-md" ref={containerRef}>
+              <Link to={`/store/${store._id}`} className="w-32 h-32">
+                <img
+                  src={store.images}
+                  alt={store.name}
+                  className="w-full h-full object-cover rounded-md shadow-lg"
+                />
+              </Link>
+              <div className="ml-4 flex-grow">
+                <h3 className="text-lg lg:text-xl xl:text-2xl text-cbb26a font-bold mb-2 overflow-hidden overflow-ellipsis max-w-xs break-all">
+                  {store.name}
+                </h3>
+                <p className="text-gray-600 mb-2 text-sm">{store.phonenumbers}</p>
+                <a
+                  href={store.website}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-blue-500 text-sm"
+                >
+                  Visit Store
+                </a>
+              </div>
             </div>
           </div>
         ))}
