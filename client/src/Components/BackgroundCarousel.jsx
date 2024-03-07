6c6b004f188c609b881799df91e01db78ddabdbf
Ben84FFM <149787249+Ben84FFM@users.noreply.github.com>
2024-03-07 17:02:08 +0100

    bufgfixingSearchBUtton

---
+++ client/src/Components/BackgroundCarousel.jsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useRef } from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+const BackgroundCarousel = () => {
+  const localImages = [
+    '../src/assets/LandingPage.jpg',
+    '../src/assets/LandingPageLogo2.png',
+  ];
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    if (containerRef.current) {
+      const maxHeight = Math.max(
+        ...Array.from(containerRef.current.children).map((child) => child.clientHeight)
+      );
+      Array.from(containerRef.current.children).forEach(
+        (child) => (child.style.height = `${maxHeight}px`)
+      );
+    }
+  }, []);
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 800,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+  };
+
+  return (
+    <div className="text-center px-4 lg:px-0 py-8">
+      <Slider {...settings} className="mx-auto max-w-screen-xl" ref={containerRef}>
+        {localImages.map((image, index) => (
+          <div key={index} className="flex items-center px-4">
+            <div className="flex bg-white rounded-md p-6 shadow-md">
+              <img
+                src={image}
+                alt={`Technology ${index + 1}`}
+                className="w-full h-auto object-cover rounded-md"
+              />
+            </div>
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
+};
+
+export default BackgroundCarousel;
--- client/src/Components/addStore.jsx
+++ client/src/Components/addStore.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
+
 const AddStoreForm = () => {
   const [storeName, setStoreName] = useState('');
   const [street, setStreet] = useState('');
