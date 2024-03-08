d8f631ba5ec8adc366df91c0013b0f58e2c90de8
Fern2023 <fernando.guarnizo@gfn.education>
2024-03-08 11:34:36 +0100

    StoreDetail Styling and Layout Design

--- client/src/Components/StoreDetail.jsx
+++ client/src/Components/StoreDetail.jsx
@@ -50,66 +50,60 @@ const StoreDetails = () => {
 
 
   return (
-    <div className="bg-black text-white">
-      <div className="containerBG bg-black bg-opacity-70 flex flex-col items-center rounded-xl p-8 lg:p-16">
-        <div className="flex flex-row lg:flex-row items-center justify-center mb-8 lg:mb-0">
 
+    <div className="containerBG flex flex-col bg-black bg-opacity-100">
 
+      <div className='p-8'>
+        <h2 className="text-2xl lg:text-3xl xl:text-4xl font-bold text-cbb26a mb-4">{store.store.name}</h2>
+        <p className="text-cbb26a mb-2">
+          {`${store.store.address.street}, ${store.store.address.zipCode} ${store.store.address.city}`}</p>
+        <a href={store.store.website}><p className="text-cbb26a mb-2">{store.store.website}</p></a>
+        <p className="text-cbb26a mb-4">{store.store.phoneNumber}</p>
+      </div>
 
-          <div className="w-full lg:w-1/2 lg:mr-4 bg-black bg-opacity-80 p-8 mb-4 lg:mb-0 rounded-md shadow-lg">
-            <h2 className="text-2xl lg:text-3xl xl:text-4xl font-bold text-cbb26a mb-4">
-              {store.store.name}
-            </h2>
-            <p className="text-cbb26a mb-2">
-              {`${store.store.address.street}, ${store.store.address.zipCode} ${store.store.address.city}`}
-            </p>
-            <p className="text-cbb26a mb-2">{store.store.website}</p>
-            <p className="text-cbb26a mb-4">{store.store.phoneNumber}</p>
-            <img
-              src={store.store.images}
-              alt={store.store.name}
-              className="w-full rounded-md shadow-lg"
-            />
-            <h3 className="text-xl lg:text-2xl xl:text-3xl font-bold text-cbb26a mb-4">
-              Reviews from Users
-            </h3>
-            <h3 className="text-xl lg:text-2xl xl:text-3xl font-bold text-cbb26a mb-4">Reviews from Users</h3>
-            <ul className="list-disc">
-              <li className="mb-2 text-cbb26a font-bold">Beste Burger in der Stadt!</li>
-              <li className="mb-2 text-cbb26a font-bold">Fantastischer Service!</li>
-            </ul>
-          </div>
-          <div className="w-full lg:w-1/2 p-8">
-          </div>
+      <div className="flex md:flex-row sm:flex-col flex-col justify-between ">
 
-          <div className="w-full lg:w-1/2 p-8">
-            <MapContainer zoom={12} style={{ height: '400px', width: '100%', borderRadius: '10px', border: '2px solid  #b8860b', }} center={store.store.location.coordinates}>
-              <TileLayer url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' />
-              <Marker position={store.store.location.coordinates}></Marker>
-            </MapContainer>
-          </div>
+        <div className="w-full p-8 ">
+          <img className="w-full rounded-md " src={store.store.images} alt={store.store.name}
+            style={{ height: '100%', width: '100%', objectFit: 'cover', }} />
         </div>
 
-        <div className="flex flex-col lg:flex-row items-center justify-center">
-          <div className="w-full lg:w-1/2 p-8 lg:ml-4 lg:block">
-            <h3 className="text-xl lg:text-2xl xl:text-3xl font-bold text-cbb26a mb-4 lg:mb-6">
-              Rating: <span className="text-yellow-400">⭐⭐⭐⭐</span>
-            </h3>
-          </div>
-
-
-          <div className="w-full lg:w-1/2 p-8 lg:ml-4 hidden lg:block">
-
-            <Link to="/reviewStore">
-              <button className="border-2 border-cbb26a text-cbb26a px-6 py-2 rounded-md mt-4 hover:bg-cbb26aHover">
-                Rate this store
-              </button>
-            </Link>
-          </div>
+        <div className="w-full p-8 ">  {/* Map */}
+          <MapContainer zoom={12} style={{ height: '100%', width: '100%', borderRadius: '10px', border: '2px solid  #b8860b', }} center={store.store.location.coordinates}>
+            <TileLayer url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' />
+            <Marker position={store.store.location.coordinates}></Marker>
+          </MapContainer>
         </div>
       </div>
 
+      <div className="flex">
+
+        <div className='fer p-8 flex-1'>
+          <h3 className="text-xl lg:text-2xl font-bold text-cbb26a">Reviews from Users</h3>
+          <ul className="list-disc">
+            <ol className="text-cbb26a font-bold">PlaceHolder!</ol>
+            <ol className="text-cbb26a font-bold">PlaceHolder!</ol>
+          </ul>
+        </div>
+
+        <div className='fer p-8 flex-1'>
+
+          <div className=" p-8 ">
+            <h3 className="text-xl font-bold text-cbb26a mt-4">Rating:<span >⭐⭐⭐⭐</span></h3>
+          </div>
+          <div className="  p-8">
+            <Link to="/reviewStore">
+              <button className="text-xl font-bold border-2 text-cbb26a mt-4 p-2 border-cbb26a rounded-md hover:bg-cbb26aHover">
+                Rate this store</button>
+            </Link>
+          </div>
+
+        </div>
+
+      </div>
+
     </div>
+
   );
 };
 
