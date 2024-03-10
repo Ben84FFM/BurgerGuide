203eb1590e0713da93805da260d8837c73330202
Ben84FFM <149787249+Ben84FFM@users.noreply.github.com>
2024-03-10 11:30:16 +0100

    ImplementTechMockUpAboutUs

---
+++ client/src/Components/AboutusMockup.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import StackCarousel from './StackCarousel';
+
+const TechStCarouselButton = () => {
+  const [showContactForm, setShowContactForm] = useState(false);
+
+  const handleContactButtonClick = () => {
+    setShowContactForm(true);
+  };
+
+  const handleCloseContactForm = () => {
+    setShowContactForm(false);
+  };
+
+  return (
+    <div className="container mx-auto p-2 max-w-full">
+      <h1 className="text-xs mb-2 text-cbb26a font-bold">Our TechStack we used to build BurgerGuide</h1>
+      <button
+        onClick={handleContactButtonClick}
+        className="bg-black font-bold text-cbb26a text-center py-2 px-4 rounded-md transition duration-300 hover:bg-gray-800"
+      >
+        SHOW
+      </button>
+
+      {showContactForm && (
+      <div className="fixed inset-0 flex items-center justify-center">
+      <div className="absolute inset-0 bg-white opacity-50"></div>
+      <div className="relative max-w-full sm:max-w-md mx-auto bg-black rounded-md p-6 opacity-70">
+        <button
+          onClick={handleCloseContactForm}
+          className="absolute top-0 right-0 p-2 text-cbb26a text-sm font-bold"
+        >
+          Close
+        </button>
+        <StackCarousel />
+   
+      </div>
+    </div>
+      )}
+    </div>
+  );
+};
+
+export default TechStCarouselButton;
--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLinkedin, faInstagram, faFacebook } from '@fortawesome/free-brands-svg-icons';
 
+
 const Footer = () => {
   const currentYear = new Date().getFullYear();
 
