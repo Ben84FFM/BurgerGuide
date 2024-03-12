eef5edaf1ad1cac63f44897d30445f639ec0abac
Fern2023 <fernando.guarnizo@gfn.education>
2024-03-12 12:30:35 +0100

    Search2

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -21,62 +21,41 @@ function Navbar() {
   };
 
   return (
-    <div className='container flex h-24 bg-gradient-to-r from-black via-zinc-800 to-black p-4 text-base  text-cbb26a relative z-10 NavbarBorder'>
-      <div className='flex-1 flex flex-row'>
-        <div className='flex flex-col items-center md:flex-row'>
-          <Link
-            to='/'
-            className='cursor-pointer hover:opacity-80 sm:p-4 text-lg uppercase font-bold'
-          >
-            Home
-          </Link>
-          <Link
-            to='/burgerstores'
-            className='cursor-pointer hover:opacity-80 sm:p-4 text-lg uppercase font-bold '
-          >
-            Stores
-          </Link>
+    <div className='container flex flex-col h-72 lg:flex-row lg:h-32 bg-gradient-to-r from-black via-zinc-800 to-black p-4 text-base  text-cbb26a relative z-10 NavbarBorder '>
+
+      <div className=' flex-1 flex flex-col xl:flex-row 
+      text-lg uppercase font-bold pt-4 '>
+        <div className='flex justify-center items-center '>
+          <Link to='/' className=' px-4 cursor-pointer hover:opacity-80  '>Home</Link>
+          <Link to='/burgerstores' className=' px-4 cursor-pointer hover:opacity-80   '>Stores</Link>
         </div>
 
-        <div className='flex relative z-1 '>
-          {' '}
-          <Search />
-        </div>
+        <div className='flex justify-center relative z-1 pt-4'>{' '}<Search /></div>
       </div>
 
-      <div className=' bg-center flex items-center '>
-        <h1
-          className='text-6xl font-serif '
-          style={{ transform: 'scalex(0.7)' }}
-        >
-          BurgerGuide
-        </h1>
-      </div>
 
-      <div className='flex-1 flex justify-end items-center'>
-        <Link
-          to='/about'
-          className='cursor-pointer hover:opacity-80 sm:p-4 md:pr-12 text-lg uppercase font-bold'
-        >
-          ABOUT US
-        </Link>
+
+
+
+
+
+
+
+
+
+
+
+      <div className=' bg-center flex items-center justify-center '><h1 className='text-6xl font-serif ' style={{ transform: 'scalex(0.7)' }}>BurgerGuide</h1></div>
+
+      <div className=' flex-1 flex justify-center items-center flex-col xl:flex-row text-lg uppercase font-bold pr-4'>
+        <Link to='/about' className=' px-4  cursor-pointer hover:opacity-80   '>ABOUT US</Link>
         {isLoggedIn ? (
-          <div className='flex flex-col  md:flex-row pr-8 text-lg uppercase font-bold'>
-            <p className='pr-8'>Welcome {userData.firstName}</p>
-            <button
-              className='cursor-pointer hover:opacity-80'
-              onClick={handleLogout}
-            >
-              LOGOUT
-            </button>
+          <div className='flex flex-col  lg:flex-row '>
+            <p className='px-4'>Welcome {userData.firstName}</p>
+            <button className='px-4  cursor-pointer hover:opacity-80' onClick={handleLogout}>LOGOUT</button>
           </div>
         ) : (
-          <Link
-            to='/login'
-            className='cursor-pointer hover:opacity-80 text-lg uppercase font-bold'
-          >
-            LOGIN
-          </Link>
+          <Link to='/login' className='cursor-pointer hover:opacity-80'>LOGIN</Link>
         )}
       </div>
     </div>
--- client/src/Components/SearchTest.jsx
+++ client/src/Components/SearchTest.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import StoreCarouselSearch from './StoreCarouselSearch';
 import PolicyBackground from './DynamicBackground';
+import Search from './Search';
 
 const SearchRestaurant = () => {
 
   return (
     <PolicyBackground>
-      <div className="min-h-screen flex  justify-center pt-8 pb-8">
-        <div className="lg:w-1/2  ">
+      <div className="container min-h-screen flex justify-center p-12 flex-col lg:flex-row ">
+
+        <div className="flex-1  p-12 ">
           <StoreCarouselSearch />
         </div>
 
+        <div className='flex-1  flex justify-center items-center pl-12 '>
+          {' '}
+          <div className=''>
+            <Search />
+          </div>
+        </div>
       </div>
     </PolicyBackground>
   );
