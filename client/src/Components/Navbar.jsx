ceb8c60982d3e304dc7d89a126a43e672c0082ae
unknown <onureredo@yahoo.com>
2024-03-12 14:32:35 +0100

    bugFix

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { Link } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import { useAuth } from '../context/AuthProvider';
-import Search from './Search';
+import SearchNavBar from './SearchNavBar';
 
 function Navbar() {
   const { isLoggedIn, setIsLoggedIn, userData } = useAuth();
@@ -22,40 +22,55 @@ function Navbar() {
 
   return (
     <div className='container flex flex-col h-72 lg:flex-row lg:h-32 bg-gradient-to-r from-black via-zinc-800 to-black p-4 text-base  text-cbb26a relative z-10 NavbarBorder '>
-
-      <div className=' flex-1 flex flex-col xl:flex-row 
-      text-lg uppercase font-bold pt-4 '>
+      <div
+        className=' flex-1 flex flex-col xl:flex-row 
+      text-lg uppercase font-bold pt-4 '
+      >
         <div className='flex justify-center items-center '>
-          <Link to='/' className=' px-4 cursor-pointer hover:opacity-80  '>Home</Link>
-          <Link to='/burgerstores' className=' px-4 cursor-pointer hover:opacity-80   '>Stores</Link>
+          <Link to='/' className=' px-4 cursor-pointer hover:opacity-80  '>
+            Home
+          </Link>
+          <Link
+            to='/burgerstores'
+            className=' px-4 cursor-pointer hover:opacity-80   '
+          >
+            Stores
+          </Link>
         </div>
 
-        <div className='flex justify-center relative z-1 pt-4'>{' '}<Search /></div>
+        <div className='flex justify-center relative z-1 pt-4'>
+          {' '}
+          <SearchNavBar />
+        </div>
       </div>
 
-
-
-
-
-
-
-
-
-
-
-
-
-      <div className=' bg-center flex items-center justify-center '><h1 className='text-6xl font-serif ' style={{ transform: 'scalex(0.7)' }}>BurgerGuide</h1></div>
+      <div className=' bg-center flex items-center justify-center '>
+        <h1
+          className='text-6xl font-serif '
+          style={{ transform: 'scalex(0.7)' }}
+        >
+          BurgerGuide
+        </h1>
+      </div>
 
       <div className=' flex-1 flex justify-center items-center flex-col xl:flex-row text-lg uppercase font-bold pr-4'>
-        <Link to='/about' className=' px-4  cursor-pointer hover:opacity-80   '>ABOUT US</Link>
+        <Link to='/about' className=' px-4  cursor-pointer hover:opacity-80   '>
+          ABOUT US
+        </Link>
         {isLoggedIn ? (
           <div className='flex flex-col  lg:flex-row '>
             <p className='px-4'>Welcome {userData.firstName}</p>
-            <button className='px-4  cursor-pointer hover:opacity-80' onClick={handleLogout}>LOGOUT</button>
+            <button
+              className='px-4  cursor-pointer hover:opacity-80'
+              onClick={handleLogout}
+            >
+              LOGOUT
+            </button>
           </div>
         ) : (
-          <Link to='/login' className='cursor-pointer hover:opacity-80'>LOGIN</Link>
+          <Link to='/login' className='cursor-pointer hover:opacity-80'>
+            LOGIN
+          </Link>
         )}
       </div>
     </div>
---
+++ client/src/Components/SearchNavBar.jsx
@@ -0,0 +1,114 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
+const SearchNavBar = () => {
+  const [search, setSearch] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [visibleResults, setVisibleResults] = useState(5);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        if (search.trim() === '') {
+          setSearchResults([]);
+          setLoading(false);
+          return;
+        }
+        const response = await axios.get(
+          `http://localhost:5000/stores/search?store=${search}`,
+          { withCredentials: true }
+        );
+        setSearchResults(response.data);
+        setLoading(false);
+        setVisibleResults(5);
+      } catch (error) {
+        console.error(error);
+        setError(true);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [search]);
+
+  const handleLoadMore = () => {
+    setVisibleResults((prevVisibleResults) => prevVisibleResults + 5);
+  };
+
+  const handleInputChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className='flex flex-col text-cbb26a '>
+      <div className=' '>
+        <form
+          onSubmit={handleSubmit}
+          className='flex flex-row items-center space-x-2'
+        >
+          <input
+            type='text'
+            value={search}
+            onChange={handleInputChange}
+            placeholder='Search...'
+            className='flex-grow px-3 py-2  rounded-md focus:outline-none focus:border-cbb26a mb-2 text-sm'
+          />
+          <button
+            type='submit'
+            className='bg-black px-2 py-2 mb-2 rounded-md hover:bg-opacity-80 focus:outline-none scale-125 '
+          >
+            <FontAwesomeIcon icon={faSearch} />
+          </button>
+        </form>
+
+        {loading && <p>Loading...</p>}
+        {error && <p>Error loading results.</p>}
+      </div>
+
+      {searchResults.length > 0 && (
+        <div className=' '>
+          <h2 className=''>Your Results</h2>
+          <ul>
+            {searchResults.slice(0, visibleResults).map((result) => (
+              <Link
+                to={`/store/${result._id}`}
+                key={result._id}
+                className='bg-white block mx-auto rounded-xl mb-4 p-4'
+              >
+                {result.name}
+              </Link>
+            ))}
+          </ul>
+          {searchResults.length > visibleResults && (
+            <button
+              className='bg-white px-4 py-2 rounded-md hover:bg-opacity-80 focus:outline-none  '
+              onClick={handleLoadMore}
+            >
+              Load More
+            </button>
+          )}
+          <div className='text-cbb26a text-center mt-4'>
+            <Link
+              to='/addRestaurant'
+              className='inline-block bg-white text-cbb26a font-bold px-4 py-2 rounded-md transition duration-300 ease-in-out hover:bg-black hover:text-cbb26a focus:outline-none shadow-md border border-gray-500'
+            >
+              Your Favorite Store is not here?
+            </Link>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default SearchNavBar;
