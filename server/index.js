cf22a3d9e7898310162fe0c56a6a2a693cae4973
Ben84FFM <149787249+Ben84FFM@users.noreply.github.com>
2024-03-13 13:48:23 +0100

    deployment

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ import { errorHandler } from './middlewares/ErrorHandler.js';
 const app = express();
 const PORT = 5000;
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: 'https://burgerguide.netlify.app', credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
